0d16840f2dce0ba98356a51326621a0d677cc081
Andy Ruan <z5590336@ad.unsw.edu.au>
2025-01-03 22:19:08 +1100

    used javascript to load the guppy grid

---
+++ scripts/data/fish_products.js
@@ -0,0 +1,32 @@
+export let fish = [
+    {
+        image: 'images/tuxedo-guppy.jpg',
+        name: 'Male Tuxedo Guppy',
+        priceCents: 2000
+    },
+    {
+        image: 'images/dumbo-mosaic-guppy.jpg',
+        name: 'Male Dumbo Mosiac Guppy',
+        priceCents: 2000
+    },
+    {
+        image: 'images/endler-guppy.jpg',
+        name: 'Endler Guppy',
+        priceCents: 1500
+    },
+    {
+        image: 'images/female-mutt-guppy.jpg',
+        name: 'Female Mutt Guppy',
+        priceCents: 1000
+    },
+    {
+        image: 'images/female-koi-guppy.jpeg',
+        name: 'Female Koi Guppy',
+        priceCents: 1500
+    },
+    {
+        image: 'images/red-albino-guppy.jpg',
+        name: 'Male Red Albino Guppy',
+        priceCents: 2000
+    }
+];
---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import { fish } from "./data/fish_products.js";
+
+
+
+console.log("hi")
+
+let gridHTML = '';
+
+fish.forEach((fish) => {
+    console.log(fish)
+    gridHTML += 
+    `
+        <div class="guppy-info">
+                <img src=${fish.image}>
+                <div class="guppy-description">
+                    <p>${fish.name}</p>
+                    <p>$${fish.priceCents / 100}</p>
+                    <button>Add to cart</button>
+                </div>
+            </div>
+    `
+})
+
+
+const res = document.querySelector('.guppy-grid').innerHTML = gridHTML
+console.log(res);
+
