a47982dbef9ea4b284f9561a1c8d83a72fe1709a
Andy <andy.ruan2005@gmail.com>
2025-01-18 19:39:17 +1100

    update gitignore and add functionality to buttons

--- scripts/data/fish_products.js
+++ scripts/data/fish_products.js
@@ -1,32 +1,42 @@
+
+
+let idCounter = 0
+
 export let fish = [
     {
         image: 'images/tuxedo-guppy.jpg',
         name: 'Male Tuxedo Guppy',
-        priceCents: 2000
+        priceCents: 2000,
+        id: idCounter++,
     },
     {
         image: 'images/dumbo-mosaic-guppy.jpg',
         name: 'Male Dumbo Mosiac Guppy',
-        priceCents: 2000
+        priceCents: 2000,
+        id: idCounter++,
     },
     {
         image: 'images/endler-guppy.jpg',
         name: 'Endler Guppy',
-        priceCents: 1500
+        priceCents: 1500,
+        id: idCounter++,
     },
     {
         image: 'images/female-mutt-guppy.jpg',
         name: 'Female Mutt Guppy',
-        priceCents: 1000
+        priceCents: 1000,
+        id: idCounter++,
     },
     {
         image: 'images/female-koi-guppy.jpeg',
         name: 'Female Koi Guppy',
-        priceCents: 1500
+        priceCents: 1500,
+        id: idCounter++,
     },
     {
         image: 'images/red-albino-guppy.jpg',
         name: 'Male Red Albino Guppy',
-        priceCents: 2000
+        priceCents: 2000,
+        id: idCounter++,
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,46 @@
 import { fish } from "./data/fish_products.js";
 
-console.log("hi")
 
-let gridHTML = '';
 
-fish.forEach((fish) => {
-    console.log(fish)
-    gridHTML += 
-    `
-        <div class="guppy-info">
-                <img src=${fish.image}>
-                <div class="guppy-description">
-                    <p>${fish.name}</p>
-                    <p>$${fish.priceCents / 100}</p>
-                    <button class="add-to-cart-btn" data-fish-id=${fish.id}>Add to cart</button>
+function renderGuppyHTML() {
+    let gridHTML = '';
+
+    fish.forEach((fish) => {
+        gridHTML += 
+        `
+            <div class="guppy-info">
+                    <img src=${fish.image}>
+                    <div class="guppy-description">
+                        <p>${fish.name}</p>
+                        <p>$${fish.priceCents / 100}</p>
+                        <button class="add-to-cart-btn" data-fish-id=${fish.id}>Add to cart</button>
+                    </div>
                 </div>
-            </div>
-    `
+        `
+    })
+
+    document.querySelector('.guppy-grid').innerHTML = gridHTML
+}
+
+
+
+renderGuppyHTML();
+
+const buttons = document.querySelectorAll('.add-to-cart-btn');
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const fishId = parseInt(event.target.dataset.fishId);
+        const targetFish = fish.find(f => f.id === fishId);
+        alert(`${targetFish.name} added to cart!`);
+
+    })
 })
 
 
-const res = document.querySelector('.guppy-grid').innerHTML = gridHTML
-console.log(res);
+
+
+
 
 
 
