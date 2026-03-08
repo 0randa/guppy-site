e08ddb0cf18bdb6e2ab2c809c95267df2903ff91
Andy <andy.ruan2005@gmail.com>
2026-03-08 20:52:18 +1100

    install mantine

--- src/cart-main.jsx
+++ src/cart-main.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import { CartPage } from "./pages/CartPage.jsx";
+import "@mantine/core/styles.css";
 import "../styles/general.css";
 import "../styles/header.css";
 import "../styles/sidebar.css";
@@ -7,4 +9,8 @@ import "../styles/guppy-grid.css";
 import "../styles/website_layout.css";
 import "../styles/cart_page.css";
 
-createRoot(document.getElementById("root")).render(<CartPage />);
+createRoot(document.getElementById("root")).render(
+  <MantineProvider>
+    <CartPage />
+  </MantineProvider>
+);
--- src/home-main.jsx
+++ src/home-main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import { HomePage } from "./pages/HomePage.jsx";
+import "@mantine/core/styles.css";
 import "../styles/general.css";
 import "../styles/header.css";
 import "../styles/sidebar.css";
 import "../styles/guppy-grid.css";
 import "../styles/website_layout.css";
 
-createRoot(document.getElementById("root")).render(<HomePage />);
+createRoot(document.getElementById("root")).render(
+  <MantineProvider>
+    <HomePage />
+  </MantineProvider>
+);
